550fcecc14ab81402615a7a3bbc7bfabeb921506
TeitokuRoss <64163929+TeitokuRoss@users.noreply.github.com>
2022-08-31 22:50:25 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace Cursash2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 10D);
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint2 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 5D);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(37, 58);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(159, 116);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(217, 58);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(159, 116);
+            this.listBox2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "019",
+            "022",
+            "035",
+            "051",
+            "052",
+            "053",
+            "054",
+            "061",
+            "071",
+            "073",
+            "075",
+            "076",
+            "081",
+            "121",
+            "122",
+            "133"});
+            this.comboBox1.Location = new System.Drawing.Point(535, 58);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "2018",
+            "2019",
+            "2020",
+            "2021"});
+            this.comboBox2.Location = new System.Drawing.Point(535, 107);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(160, 24);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(495, 210);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 28);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Оновити статистику";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(16, 182);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series1.Points.Add(dataPoint1);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series2.Points.Add(dataPoint2);
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(400, 369);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(531, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Спеціальність";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(531, 87);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Рік";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(531, 137);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Навчальний рівень";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Бакалвїавр",
+            "Магістр",
+            "Молодший бакалавр ",
+            "Фаховий молодший бакалавр"});
+            this.comboBox3.Location = new System.Drawing.Point(535, 156);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(160, 24);
+            this.comboBox3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(87, 38);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "ЗНУ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(259, 38);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "ЗПУ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1187, 640);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+using System.Text.Json.Serialization;
+//using Windows.Web.Http;
+using System.IO.Compression;
+using System.Text.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Cursash2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static async Task<string> func(string spec, string year, int un,int qua,int bas)
+        {
+            var handler = new HttpClientHandler();
+            handler.UseCookies = false;
+
+            // In production code, don't destroy the HttpClient through using, but better reuse an existing instance
+            // https://www.aspnetmonsters.com/2016/08/2016-08-27-httpclientwrong/
+            using (var httpClient = new HttpClient(handler))
+            {
+                string site;
+                if (year != "2021")
+                    site = year + ".edbo.gov.ua/offers/?qualification=1&education-base=40&speciality=121&education-form=1&course=1&university-name=" + un;
+                else
+                    site = year + ".edbo.gov.ua/offers-universities/";
+                using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://vstup" + site))
+                {
+                    request.Headers.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0");
+                    request.Headers.TryAddWithoutValidation("Accept", "application/json, text/javascript, */*; q=0.01");
+                    request.Headers.TryAddWithoutValidation("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3");
+                    request.Headers.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate, br");
+                    request.Headers.TryAddWithoutValidation("X-Requested-With", "XMLHttpRequest");
+                    request.Headers.TryAddWithoutValidation("Origin", "https://vstup" + year + ".edbo.gov.ua");
+                    request.Headers.TryAddWithoutValidation("Connection", "keep-alive");
+                    request.Headers.TryAddWithoutValidation("Referer", "https://vstup" + year + ".edbo.gov.ua/offers/?qualification=1&education-base=40&speciality=121&education-form=1&course=1&university-name=" + un);
+                    request.Headers.TryAddWithoutValidation("Sec-Fetch-Dest", "empty");
+                    request.Headers.TryAddWithoutValidation("Sec-Fetch-Mode", "cors");
+                    request.Headers.TryAddWithoutValidation("Sec-Fetch-Site", "same-origin");
+                    request.Headers.TryAddWithoutValidation("Cookie", "_ga=GA1.1.2041819962.1638457784; _ga_W6WT1K3VXZ=GS1.1.1644952579.3.1.1644952591.0; _ga_YC32TV7WL7=GS1.1.1644952590.4.1.1644952753.0; _ga_96Q7K30V0N=GS1.1.1643115886.4.0.1643115888.0; PHPSESSID=sof0jmp661095i2grv7rhbha8a");
+                    if (year != "2021")
+                        request.Content = new StringContent("action=universities&university-id=&qualification="+qua+"&education-base="+bas+"&speciality=" + spec + "&program=&education-form=1&course=1&region=&university-name=" + un);
+                    else
+                        request.Content = new StringContent("qualification="+qua+"&education_base="+bas+"&speciality=" + spec + "&region=80&university=" + un + "&study_program=&education_form=&course=");
+                    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded; charset=UTF-8");
+
+
+                    var response = await httpClient.SendAsync(request);
+                    response.EnsureSuccessStatusCode();
+                    response.Content.ReadAsStringAsync().Wait();
+                    var responseBody = response.Content.ReadAsStreamAsync().Result;
+                    Stream decompressed = new GZipStream(responseBody, CompressionMode.Decompress);
+                    StreamReader objReader = new StreamReader(decompressed, Encoding.UTF8);
+                    //var response = JsonConvert.DeserializeObject<T>(responseBody);
+                    var resp = objReader.ReadToEnd();
+                    return resp;
+                }
+            }
+        }
+        static async Task<string> func2(string codes, string year, int un)
+        {
+            var handler = new HttpClientHandler();
+            handler.UseCookies = false;
+
+            // In production code, don't destroy the HttpClient through using, but better reuse an existing instance
+            // https://www.aspnetmonsters.com/2016/08/2016-08-27-httpclientwrong/
+            using (var httpClient = new HttpClient(handler))
+            {
+                string site;
+                if (year != "2021")
+                    site = year + ".edbo.gov.ua/";
+                else
+                    site = year + ".edbo.gov.ua/offers-list/";
+                using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://vstup" + site))
+                {
+                    request.Headers.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0");
+                    request.Headers.TryAddWithoutValidation("Accept", "application/json, text/javascript, */*; q=0.01");
+                    request.Headers.TryAddWithoutValidation("Accept-Language", "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3");
+                    request.Headers.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate, br");
+                    request.Headers.TryAddWithoutValidation("X-Requested-With", "XMLHttpRequest");
+                    request.Headers.TryAddWithoutValidation("Origin", "https://vstup" + year + ".edbo.gov.ua");
+                    request.Headers.TryAddWithoutValidation("Connection", "keep-alive");
+                    request.Headers.TryAddWithoutValidation("Referer", "https://vstup" + year + ".edbo.gov.ua/offers/?qualification=1&education-base=40&speciality=121&university-name=" + un);
+                    request.Headers.TryAddWithoutValidation("Sec-Fetch-Dest", "empty");
+                    request.Headers.TryAddWithoutValidation("Sec-Fetch-Mode", "cors");
+                    request.Headers.TryAddWithoutValidation("Sec-Fetch-Site", "same-origin");
+                    request.Headers.TryAddWithoutValidation("Cookie", "_ga=GA1.1.2041819962.1638457784; _ga_W6WT1K3VXZ=GS1.1.1644952579.3.1.1644952591.0; _ga_YC32TV7WL7=GS1.1.1644952590.4.1.1644952753.0; _ga_96Q7K30V0N=GS1.1.1643115886.4.0.1643115888.0; PHPSESSID=sof0jmp661095i2grv7rhbha8a");
+
+                    if (year != "2021")
+                        request.Content = new StringContent("action=offers&usids=" + codes);
+                    else
+                        request.Content = new StringContent("ids=" + codes);
+                    request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded; charset=UTF-8");
+
+
+
+                    var response = await httpClient.SendAsync(request);
+                    response.EnsureSuccessStatusCode();
+                    response.Content.ReadAsStringAsync().Wait();
+                    var responseBody = response.Content.ReadAsStreamAsync().Result;
+                    Stream decompressed = new GZipStream(responseBody, CompressionMode.Decompress);
+                    StreamReader objReader = new StreamReader(decompressed, Encoding.UTF8);
+                    //var response = JsonConvert.DeserializeObject<T>(responseBody);
+                    var resp = objReader.ReadToEnd();
+                    return resp;
+                }
+            }
+        }
+        private async void writeout(string year, string spec, int un,int qua,int bas, int first, int second, ListBox l)
+        {
+            var result = await func(spec, year, un,qua,bas);
+            //MessageBox.Show(result);
+            if (comboBox2.SelectedItem.ToString() == "2021")
+            {
+                var results = result.Split('"');
+                if (results.Length < second)
+                {
+                    l.Items.Add("Конкурсні пропозиції не знайдені");
+                    return;
+                }
+                result = results[second];
+                //MessageBox.Show(result, second.ToString());
+            }
+            else
+            {
+                var results = result.Split('"');
+                if (results.Length < first)
+                {
+                    l.Items.Add("Конкурсні пропозиції не знайдені");
+                    return;
+                }
+                result = results[first];
+                //MessageBox.Show(result, second.ToString());
+            }
+            string[] codes = result.Split(',');
+            result = await func2(result, year, un);
+            //MessageBox.Show(result);
+            var resp = JsonConvert.DeserializeObject<JToken>(result);
+            //MessageBox.Show(resp["offers_requests_info"].ToString());
+            if (year != "2021") 
+            foreach (string c in codes)
+            {
+                var numbers = resp["offers_requests_info"][c];
+                    //MessageBox.Show(resp["offers_requests_info"].ToString());
+                    //MessageBox.Show(resp["offers_requests_info"][c].ToString());
+                l.Items.Add("Сер:" + numbers[2].ToString() + " Мин:" + numbers[3].ToString() + " Макс:" + numbers[4].ToString());
+            }
+            else 
+            {
+                var respi = resp["offers"];
+                //MessageBox.Show(respi.ToString());
+                for(int i=0;i<codes.Length;i++)
+                {
+                    //MessageBox.Show(respi[i]["st"].ToString());
+                    if (respi[i]["st"]!=null)
+                    {
+                        var numbers = respi[i]["st"]["c"];
+                        l.Items.Add("Сер:" + numbers["rm"].ToString() + " Мин:" + numbers["obm"].ToString() + " Макс:" + numbers["ocm"].ToString());
+                    }
+                    
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string year = comboBox2.SelectedItem.ToString();
+            string spec = comboBox1.SelectedItem.ToString();
+            int qua = comboBox3.SelectedIndex;
+            int qualification=0;
+            int[] bases ={ 40,320,520};
+            switch (qua)
+            {
+                case (0): qualification = 1;bases =new int[] {40,320,520};  break;
+                case (1): qualification = 2; bases = new int[] { 40, 320, 520,640 }; break;
+                case (2): qualification = 6; bases = new int[] { 40, 320}; break;
+                case (3): qualification = 9; bases = new int[] { 30, 40, 510,520 }; break;
+            }
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            foreach(int bas in bases)
+            {
+                //znu
+
+                writeout(year, spec, 73, qualification, bas, 5, 11, listBox1);
+
+                //zntu
+
+                writeout(year, spec, 91, qualification, bas, 7, 13, listBox2);
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
